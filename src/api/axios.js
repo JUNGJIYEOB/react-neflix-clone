ab12884fee26a70b2817547aca283890428b2e3a
정지엽 <wjdwuq1@gtsoft.co.kr>
2023-10-16 16:34:20 +0900

    create files

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const instance =axios.create({
+    baseURL:"http://api.themoviedb.org/3",
+    params:{
+        api_key:"a5023cf07926e1633b6a8e896408b74d",
+        language:"ko-KR"
+    }
+})
+
+export default instance
