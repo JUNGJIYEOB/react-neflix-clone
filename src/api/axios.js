063a6a1f176fd8f2a8124c085cefe38afdf31721
정지엽 <wjdwuq1@gtsoft.co.kr>
2023-10-19 11:24:44 +0900

    api-key hidden

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const instance =axios.create({
     baseURL:"http://api.themoviedb.org/3",
     params:{
-        api_key:"a5023cf07926e1633b6a8e896408b74d",
+        api_key:process.env.REACT_APP_MOVIE_DB_API_KEY,
         language:"ko-KR"
     }
 })
