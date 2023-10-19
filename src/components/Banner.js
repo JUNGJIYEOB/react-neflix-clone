3c78850d3b93893cd4c760553a361e69c5d143f0
정지엽 <wjdwuq1@gtsoft.co.kr>
2023-10-19 12:44:58 +0900

    최로 렌더링시 메인 img 없을때 404 에러 조건문 추가로 해결

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -48,7 +48,8 @@ export default function Banner(){
             <header
                 className="banner"
                 style={{
-                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+
+                    backgroundImage: movie.backdrop_path&&`url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
                     backgroundPosition: "top center",
                     backgroundSize: "cover",
                 }}
